da5f47e3b82cc5b72a1792ead8d0329b5b9e107d
ColinChan2007 <140026039+ColinChan2007@users.noreply.github.com>
2023-08-04 17:52:57 -0700

    Update Withdraw.java

--- DataAbstractionAssignment/src/Withdraw.java
+++ DataAbstractionAssignment/src/Withdraw.java
@@ -6,12 +6,18 @@ public class Withdraw {
     private Date date;
     private String account;
 
+    // Requires: amount (withdrawal amount), date (transaction date), account (checking/saving)
+    // Modifies: this (Withdraw object)
+    // Effects: Initializes a new Withdraw object with the given amount, date, and account
     Withdraw(double amount, Date date, String account) {
         this.amount = amount;
         this.date = date;
         this.account = account;
     }
 
+    // Requires: None
+    // Modifies: None
+    // Effects: Returns a formatted string representation of the withdrawal
     public String toString() {
         SimpleDateFormat sdf = new SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy");
         return "Withdraw:\nWithdrawal of: $" + amount + " Date: " + sdf.format(date) +
